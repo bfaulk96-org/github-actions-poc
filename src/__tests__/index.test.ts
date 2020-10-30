dab8d5fc546c1aca7a54374cfe54195ee9e32cb5
bfaulk96 <btfaulkner96@gmail.com>
2020-10-30 16:38:47 -0500

    Intentionally break

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -2,7 +2,7 @@ import { uselessNegate } from '../index';
 
 describe(uselessNegate, () => {
   test('should return false if true is passed in', () => {
-    expect(uselessNegate(true)).toBeFalsy();
+    expect(uselessNegate(false)).toBeFalsy();
   });
 
   test('should return true if false is passed in', () => {
