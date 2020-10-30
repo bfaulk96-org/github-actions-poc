f34f60c0ee8fa4bae53597e503c0f4fa8cc6b414
bfaulk96 <btfaulkner96@gmail.com>
2020-10-30 17:27:46 -0500

    Fix

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -2,7 +2,7 @@ import { uselessNegate } from '../index';
 
 describe(uselessNegate, () => {
   test('should return false if true is passed in', () => {
-    expect(uselessNegate(false)).toBeFalsy();
+    expect(uselessNegate(true)).toBeFalsy();
   });
 
   test('should return true if false is passed in', () => {
